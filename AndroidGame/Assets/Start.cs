2c72f079d7db7c873e48b8c54d795f46c58ce25d
Dani <daniel.mas.pedros@gmail.com>
2021-03-11 12:55:33 +0100

    aa

---
+++ AndroidGame/Assets/Start.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+    public void start()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
