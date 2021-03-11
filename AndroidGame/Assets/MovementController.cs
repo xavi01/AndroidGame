c123f89f243fb4eb3a0591fddee6090da73d7c34
xavi01 <xavier.navarro.mas@gmail.com>
2021-03-11 12:52:32 +0100

    Q1

---
+++ AndroidGame/Assets/MovementController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    public GameObject player;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Left()
+    {
+
+    }
+
+    public void Right()
+    {
+
+    }
+}
