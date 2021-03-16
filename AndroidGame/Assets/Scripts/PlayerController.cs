397487f1a794b000c021b6257fe6a80a10f39a8b
xavi01 <xavier.navarro.mas@gmail.com>
2021-03-16 17:48:48 +0100

    Updates

--- AndroidGame/Assets/Scripts/PlayerController.cs
+++ AndroidGame/Assets/Scripts/PlayerController.cs
@@ -2,30 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 
-    public GameObject player;
     public GameObject left;
     public GameObject right;
     public GameObject middle;
     private bool rightR;
     private bool leftL;
     private bool middleM;
-
-
+    //private float speed = 1;
+    private Rigidbody rb;
+    private Animator m_Animator;
 
     void Start()
     {
         middleM = true;
         rightR = false;
         leftL = false;
-
+        m_Animator = gameObject.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        rb.velocity = new Vector3(0, 0, 4);
+        m_Animator.SetTrigger("Run");
     }
 
 
@@ -34,25 +36,29 @@ public class MovementController : MonoBehaviour
         if (middleM == true)
         {
 
-            player.transform.position = new Vector3(left.transform.position.x, left.transform.position.y, left.transform.position.z);
+            transform.position = new Vector3(left.transform.position.x, left.transform.position.y, left.transform.position.z);
+            //float step = speed * Time.deltaTime;
+            //transform.position = Vector3.MoveTowards(left.position, left.position, step);
+
+
 
             Debug.Log("MOURE esquerra");
             leftL = true;
             middleM = false;
-           
+
 
         }
 
         if (rightR == true)
         {
             Debug.Log("MOURE mitg");
-            player.transform.position = new Vector3(middle.transform.position.x, middle.transform.position.y, middle.transform.position.z);
+            transform.position = new Vector3(middle.transform.position.x, middle.transform.position.y, middle.transform.position.z);
             middleM = true;
             rightR = false;
-           
+
         }
 
-        if (leftL==true)
+        if (leftL == true)
         {
 
             Debug.Log("NO ES POT POURE CAP A L'ESQUERRA");
@@ -68,20 +74,20 @@ public class MovementController : MonoBehaviour
         if (middleM == true)
         {
             Debug.Log("MOURE dreta");
-            player.transform.position = new Vector3(right.transform.position.x, right.transform.position.y, right.transform.position.z);
+            transform.position = new Vector3(right.transform.position.x, right.transform.position.y, right.transform.position.z);
             rightR = true;
             middleM = false;
-            
+
 
         }
 
         if (leftL == true)
         {
             Debug.Log("MOURE mitg");
-            player.transform.position = new Vector3(middle.transform.position.x, middle.transform.position.y, middle.transform.position.z);
+           transform.position = new Vector3(middle.transform.position.x, middle.transform.position.y, middle.transform.position.z);
             middleM = true;
             leftL = false;
-            
+
         }
 
         if (rightR == true)
---
+++ AndroidGame/Assets/Scripts/Velocidad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocidad : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0,0,4);
+    }
+}
