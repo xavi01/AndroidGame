6f39bf0c70d2fc9d47eeb9ee409a04a9013e658c
xavi01 <xavier.navarro.mas@gmail.com>
2021-04-07 17:36:27 +0200

    mroe

---
+++ AndroidGame/Assets/ObjectsLeft.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsLeft : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    public float tileLength = 30;
+    public int numberOfTiles = 6;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+    void Start()
+    {
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            if (i == 0)
+                SpawnTile(0);
+            else
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+
+    }
+    void Update()
+    {
+        if (playerTransform.position.z - 30 > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+
+    }
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        go.transform.position = new Vector3(-2, go.transform.position.y, go.transform.position.z);
+        activeTiles.Add(go);
+        zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
